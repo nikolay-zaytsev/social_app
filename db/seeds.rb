1d410038165e3a1c0637acc78de349857cd5d124
kidddMonsta <nikolas.yeap@gmail.com>
2019-06-13 19:13:51 +0300

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+
+
+User.create!(name:  "Nikolas the Programmer",
+             email: "nikolas.yeap@gmail.com",
+             password:              "qwerty",
+             password_confirmation: "qwerty",
+             admin: true)
+
+#99.times do |n|
+#  name  = Faker::Name.name
+#  email = "example-#{n+1}@railstutorial.org"
+#  password = "password"
+#  User.create!(name:  name,
+#             email: email,
+#             password:              password,
+#             password_confirmation: password)
+#end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
