29c9ab03b9c22efa3d063b0a6dc26797ab2435a5
kidddMonsta <nikolas.yeap@gmail.com>
2019-06-15 18:08:50 +0300

    Add account activation

--- app/helpers/account_activatios_helper.rb
+++
@@ -1,2 +0,0 @@
-module AccountActivatiosHelper
-end
--- test/controllers/account_activations_controller_test.rb
+++ test/controllers/account_activations_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AccountActivatiosControllerTest < ActionDispatch::IntegrationTest
+class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
